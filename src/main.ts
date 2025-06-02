d59dca13b22657c7f4b9f31ad76364925952515e
penicillin0 <ayahitonakamura@gmail.com>
2025-06-02 20:42:21 +0900

    feat: add LSUIElement to extendInfo in package.json and hide dock icon on macOS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { TrayManager } from "./tray";
 const trayManager = new TrayManager();
 
 app.whenReady().then(async () => {
+  // Hide the dock icon on macOS
+  if (process.platform === "darwin" && app.dock) {
+    app.dock.hide();
+  }
+
   await trayManager.initializeTray();
 });
 
